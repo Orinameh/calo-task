61c8d295ea897b643130ca08325008324bc247f7
Orinameh <memefacto@yahoo.com>
2021-07-21 13:30:48 +0100

    remove refrence to the map config files

--- config/index.ts
+++ config/index.ts
@@ -1,5 +1,4 @@
-import GOOGLE_MAP_KEYS from './map';
 import data from './data.json';
 import {COLORS, SIZES} from './theme';
 
-export {data, GOOGLE_MAP_KEYS, COLORS, SIZES};
+export {data, COLORS, SIZES};
