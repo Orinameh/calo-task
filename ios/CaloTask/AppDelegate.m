64e53cdf0bdddb4f3ecdfd456d7830e207f9be41
Orinameh <memefacto@yahoo.com>
2021-07-25 15:03:37 +0100

    remove api key totally

--- ios/CaloTask/AppDelegate.m
+++ ios/CaloTask/AppDelegate.m
@@ -28,7 +28,7 @@ static void InitializeFlipper(UIApplication *application) {
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  [GMSServices provideAPIKey:@"AIzaSyCVB-wBKJ_t0IaEbPEezIPMuR7QtCoBdz8"];
+  [GMSServices provideAPIKey:@"GOOGLE_API_KEY"];
 #ifdef FB_SONARKIT_ENABLED
   InitializeFlipper(application);
 #endif
